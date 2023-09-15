cf56c08b7f85a4bee48f80bb05b9e542da2de2b0
Victor Levitsky <68056117+parthian-sterlet@users.noreply.github.com>
2023-09-15 12:30:05 +0700

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
-g++ -o fasta_to_plain fasta_to_plain.cpp
+g++ -o background_genome_mono.exe background_genome_mono.cpp
+g++ -o bed_chr_mask.exe bed_chr_mask.cpp
+g++ -o bed_chr_separation.exe bed_chr_separation.cpp
+g++ -o bed_sort.exe bed_sort.cpp
+g++ -o fasta_muliplefiles.exe fasta_muliplefiles.cpp
+g++ -o fasta_to_plain0.exe fasta_to_plain0.cpp
+g++ -o longext_many.exe longext_many.cpp
 g++ -o mix0 mix0.cpp
-g++ -o background_genome_mono background_genome_mono.cpp
-chmod a+x fasta_to_plain 
-chmod a+x mix0
-chmod a+x background_genome_mono
+
+chmod a+x background_genome_mono.exe
+chmod a+x bed_chr_mask.exe
+chmod a+x bed_chr_separation.exe
+chmod a+x bed_sort.exe
+chmod a+x fasta_muliplefiles.exe
+chmod a+x fasta_to_plain0.exe
+chmod a+x longext_many.exe
+chmod a+x mix0.exe
+
