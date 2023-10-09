90301a1a4acc82713a0bbc958ad314933035ae25
Victor Levitsky <68056117+parthian-sterlet@users.noreply.github.com>
2023-10-09 08:22:24 +0700

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -7,6 +7,7 @@ g++ -o bed_sort.exe bed_sort.cpp
 g++ -o fasta_muliplefiles.exe fasta_muliplefiles.cpp
 g++ -o fasta_to_plain0.exe fasta_to_plain0.cpp
 g++ -o longext_many.exe longext_many.cpp
+g++ -o mix0.cpp mix0.exe
 
 chmod a+x area_self_overlap.exe
 chmod a+x background_genome_mono.exe
@@ -16,10 +17,8 @@ chmod a+x bed_sort.exe
 chmod a+x fasta_muliplefiles.exe
 chmod a+x fasta_to_plain0.exe
 chmod a+x longext_many.exe
-
-chmod a+x mask.pl
-chmod a+x mask_rev.pl
-chmod a+x no_mask.pl
-chmod a+x mask_run
-chmod a+x mask_rev_run
-chmod a+x no_mask_run
+chmod a+x mix0.exe
+chmod a+x mask_blacklisted.pl
+chmod a+x mask_whitelisted.pl
+chmod a+x no_mask
+chmod a+x command_line
